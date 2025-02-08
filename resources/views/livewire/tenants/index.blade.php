9afe51c6c63fc80d3b982816ae64a34a890b73f7
Spicer Matthews <spicer@cloudmanic.com>
2025-02-08 00:25:55 -0800

    Removed the X from the tenant search

--- resources/views/livewire/tenants/index.blade.php
+++ resources/views/livewire/tenants/index.blade.php
@@ -62,7 +62,7 @@
                         </path>
                     </svg>
                 </div>
-                <input wire:model.live.debounce.300ms="search" type="search"
+                <input wire:model.live.debounce.300ms="search" type="text"
                     class="block w-full rounded-md border-0 py-1.5 pl-10 pr-8 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     placeholder="Search tenants...">
                 @if($search)
