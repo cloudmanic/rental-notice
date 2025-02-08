14515a3e33db958a2e7a8a7b088ce50e30eeacd0
Spicer Matthews <spicer@cloudmanic.com>
2025-02-07 21:43:57 -0800

    Fixed layout issues.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,14 +12,15 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="font-sans antialiased h-full">
-    <div class="min-h-full bg-gray-100 flex flex-col">
-        @include('layouts._navigation')
-        <div class="flex-1 bg-gray-50">
-            <div class="py-10">
+<body class="font-sans antialiased min-h-full flex flex-col">
+    <div class="flex-1 flex flex-col bg-gray-100">
+        <div class="flex-1 flex flex-col bg-gray-50">
+            @include('layouts._navigation')
+
+            <div class="flex-1 py-6">
                 <main>
                     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                        <div class="px-4 py-8 sm:px-0">
+                        <div class="px-4 sm:px-0">
                             <div class="bg-white overflow-hidden shadow rounded-lg">
                                 <div class="px-4 py-5 sm:p-6">
                                     @yield('content')
