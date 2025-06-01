bad36b08939a23f9844f4a9eccd16cf64236a890
Spicer Matthews <spicer@cloudmanic.com>
2025-05-31 21:38:59 -0700

    Added a custom event for proceed to payment

--- resources/views/livewire/notices/preview.blade.php
+++ resources/views/livewire/notices/preview.blade.php
@@ -15,7 +15,7 @@
             </button>
 
             <a href="{{ route('stripe.checkout', $notice->id) }}"
-                class="px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                class="plausible-event-name=proceed-to-payment px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                 Proceed to Payment
             </a>
         </div>
